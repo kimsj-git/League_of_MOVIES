aa72e812d78e1fb77ebbf16bb9cb95404313a2de
kimsj <kimsj.py@gmail.com>
2022-11-16 01:02:31 +0900

    Update: backend

---
+++ final-pjt-back/movies/views.py
@@ -0,0 +1,31 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+import requests
+
+import os
+import json
+
+BASE_URL = 'https://api.themoviedb.org/3'
+
+BASE_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+secret_file = os.path.join(BASE_DIR, 'secret.json')
+with open(secret_file) as f:
+    secrets = json.load(f)
+
+API_KEY = secrets["API_KEY"]
+
+# Create your views here.
+def movies_list(request):
+    path = '/movie/popular'
+    params = {
+        'api_key': API_KEY,
+        'region': 'KR',
+        'language': 'ko',
+    }
+    response = requests.get(BASE_URL + path, params=params).json()
+    movies = response.get('results')
+    print(movies[0])
+    # return movies
+
