43eec35d3176b319ae5755e5b6ff4d5b57e0274e
kimsj <kimsj.py@gmail.com>
2022-11-25 02:40:48 +0900

    Front: 다크테마 적용..

--- final-pjt-front/src/components/LeagueListItem.vue
+++ final-pjt-front/src/components/LeagueListItem.vue
@@ -1,17 +1,29 @@
 <template>
-  <v-container @click.stop="goMatchDetail(match.pk, match.movie_1, match.movie_2)">
-    <v-row rows="auto" >
-      <v-card class="row justify-content-center p-0">
-        <MovieCard :movie="secMovie"/>
-        <!-- <p>{{ firstMovie.title }} vs {{ secMovie.title }}</p> -->
-        <MovieCard :movie="firstMovie"/>
+  <v-container
+    @click.stop="goMatchDetail(match.pk, match.movie_1, match.movie_2)"
+  >
+    <v-row rows="auto">
+      <div
+        class="row justify-content-center p-0"
+        :style="{
+          'background-image':
+            'url(https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg)',
+        }"
+      >
+        <v-row>
+          <p style="color: white;">{{ firstMovie.title }} vs {{ secMovie.title }}</p>
+        </v-row>
+        <v-row class="row justify-content-center">
+          <MovieCard :movie="secMovie" />
+          <MovieCard :movie="firstMovie" />
+        </v-row>
         <!-- <v-card class="px-0 py-0 m-3" max-width="300">
           <v-img :src="firstPoster" alt="IMG" :aspect-ratio="1 / 1.414" />
         </v-card>
         <v-card class="px-0 py-0 m-3" max-width="300">
           <v-img :src="secPoster" alt="IMG" :aspect-ratio="1 / 1.414" />
         </v-card> -->
-      </v-card>
+      </div>
       <!-- <v-col class="row justify-content-center">
       </v-col> -->
     </v-row>
@@ -20,7 +32,7 @@
 
 <script>
 // import axios from 'axios'
-import MovieCard from '@/components/MovieCard'
+import MovieCard from "@/components/MovieCard";
 
 const POSTER_URL = "https://image.tmdb.org/t/p/original";
 // const API_URL = 'http://127.0.0.1:8000/'
@@ -56,7 +68,7 @@ export default {
         name: "LeagueDetail",
         params: { match_pk, movie_1, movie_2 },
         props: true,
-      })
+      });
     },
     getMatchMovies() {
       const firstId = this.match.movie_1;
--- final-pjt-front/src/views/MatchRankingView.vue
+++ final-pjt-front/src/views/MatchRankingView.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <nav>
+    <!-- <nav>
       <router-link to="/match-ranking">리그순위</router-link> |
       <router-link to="/vote-ranking">득표순위</router-link> |
       <router-link to="/latest">최신영화</router-link>
-    </nav>
+    </nav> -->
     <router-view/>
     <MatchRankingList/>
   </div>
